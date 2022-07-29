c9b7a94aa8170ae04beabcc2812c4ae26511fed1
AwrelH <hiwa.awrel@gmail.com>
2022-07-29 06:47:22 +0000

    add degrees function and testing out validate function

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-
+def degrees():
+    """
+    Function to convert celcius to fahrenheit and vice versa.
+    """
+    print('the degree function')
 
 
 def welcome():
@@ -17,6 +21,42 @@ def welcome():
         make your choice my entering one of the above(1-4) options.
         """
     print(message)
+    while True:
+        choice = input()
+        if validate(choice):
+            if choice == '1':
+                degrees()
+            elif choice == '2':
+                print('2 = meters/feet\n')
+            elif choice == '3':
+                print('3 = kilogram/pound\n')
+            elif choice == '4':
+                print('4 = liters/us gallons\n')
+        else:
+            welcome()
+        return True
+        
+
+        
+
+
+
+def validate(values):
+    """
+    Inside try, raise error, if input not equal to,
+    1,2,3,4.
+    """
+    try:
+        int(values)
+        if values not in ('1', '2', '3', '4'):
+            raise ValueError(f"1,2,3,4 are valid values, you typed {values}")
+    except ValueError as e:
+        print(f"Invalid input: {e}, try again.\n")
+        return False
+    return True
+
+        
 
 
 welcome()
+
