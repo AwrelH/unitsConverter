c8a174af280b502263f0f1c99ff16d0921d254e2
AwrelH <hiwa.awrel@gmail.com>
2022-07-29 06:28:54 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+
+def welcome():
+    """
+    Print welcome message
+    """
+    message = """welcome to the unit converter!\n
+        Units you can convert:\n
+        1 = celcius/fahrenheit\n
+        2 = meters/feet\n
+        3 = kilogram/pound\n
+        4 = liters/us gallons\n
+        make your choice my entering one of the above(1-4) options.
+        """
+    print(message)
+
+
+welcome()
